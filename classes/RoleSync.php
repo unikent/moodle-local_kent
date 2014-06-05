4a5d34e7ce0f4fb125f5e17e4498e9e182b2656a
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-06-05 14:51:21 +0100

    Add push up base

--- classes/RoleSync.php
+++ classes/RoleSync.php
@@ -27,5 +27,127 @@ class RoleSync
      * Run the shouter cron.
      */
     public static function cron() {
+        $engine = new static();
+
+        if (get_config("local_kent", "sync_panopto")) {
+            $engine->sync_panopto();
+        }
+
+        if (get_config("local_kent", "sync_helpdesk")) {
+            $engine->sync_helpdesk();
+        }
+
+        if (get_config("local_kent", "sync_cla")) {
+            $engine->sync_cla();
+        }
+
+        if (get_config("local_kent", "sync_category_admins")) {
+            $engine->sync_category_admins();
+        }
     }
+
+    /**
+     * Grab's the ID for a given role.
+     */
+    private function get_role_id($shortname) {
+        global $DB;
+
+        return $DB->get_field('role', 'id', array(
+            'shortname' => $shortname
+        ));
+    }
+
+    /**
+     * Push a given set of roles up to SHAREDB.
+     */
+    private function push_up($roleid, $contextid) {
+        global $CFG, $DB, $SHAREDB;
+
+        // Grab users.
+        $users = $DB->get_fieldset_sql("SELECT u.username
+            FROM {role_assignments} ra
+            INNER JOIN {user} u
+              ON u.id=ra.userid
+            WHERE ra.roleid = :roleid AND ra.contextid = :contextid
+            GROUP BY u.username
+        ", array(
+            "roleid" => $roleid,
+            "contextid" => $contextid
+        ));
+
+        $role = $DB->get_record('role', array(
+            'id' => $roleid
+        ), 'id, shortname');
+
+        $params = array(
+            'moodle_env' => $CFG->kent->environment,
+            'moodle_dist' => $CFG->kent->distribution,
+            'roleid' => $role->id,
+            'shortname' => $role->shortname
+        );
+
+        // Sync the role itself.
+        if (!$SHAREDB->record_exists('shared_roles', $params)) {
+            $SHAREDB->insert_record('shared_roles', $params);
+        }
+
+        $shareid = $SHAREDB->get_field('shared_roles', 'id', $params);
+
+        $params = array(
+            'moodle_env' => $CFG->kent->environment,
+            'moodle_dist' => $CFG->kent->distribution,
+            "roleid" => $shareid
+        );
+
+        // And the assignments for this context.
+        $SHAREDB->delete_records("shared_role_assignments", $params);
+        foreach ($users as $username) {
+            $params['username'] = $username;
+            $SHAREDB->insert_record("shared_role_assignments", $params);
+        }
+    }
+
+    /**
+     * Pull a given set of roles from SHAREDB.
+     */
+    private function pull_down($roleid, $context) {
+
+    }
+
+    /**
+     * Sync panopto role between connected Moodle installations
+     */
+    private function sync_panopto() {
+        $context = \context_system::instance();
+
+        $roleid = $this->get_role_id('panopto_academic');
+        $this->push_up($roleid, $context->id);
+        $this->pull_down($roleid, $context->id);
+
+        $roleid = $this->get_role_id('panopto_non_academic');
+        $this->push_up($roleid, $context->id);
+        $this->pull_down($roleid, $context->id);
+    }
+
+    /**
+     * Sync helpdesk role between connected Moodle installations
+     */
+    private function sync_helpdesk() {
+
+    }
+
+    /**
+     * Sync cla role between connected Moodle installations
+     */
+    private function sync_cla() {
+
+    }
+
+    /**
+     * Sync category_admins role between connected Moodle installations
+     */
+    private function sync_category_admins() {
+
+    }
+
 }
