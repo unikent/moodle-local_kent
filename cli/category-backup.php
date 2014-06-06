8f312079ce83bfddb72f5b80fc6ae7ca6299290a
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-06-06 07:48:10 +0100

    Add initial Category backup script

---
+++ cli/category-backup.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file backs up all courses in a category, then gives you the tgz.
+ */
+
+define('CLI_SCRIPT', true);
+
+require_once(dirname(__FILE__) . '/../../../config.php');
+require_once($CFG->libdir . '/clilib.php');
+
+list($options, $unrecognized) = cli_get_params(
+    array(
+        'category' => false
+    )
+);
+
+if (!$options['category']) {
+    cli_error("Must specify category with --category=id!");
+}
+
+raise_memory_limit(MEMORY_HUGE);
+
+cli_heading("Running Category Backup");
