07867b4da2e90a05d8d03670642f445a644611b7
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-06-20 11:50:14 +0100

    MORE N's PLS

--- cli/destroy.php
+++ cli/destroy.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-die("You don't want to do that...\nTrust me, I'm a computer.");
+die("You don't want to do that...\nTrust me, I'm a computer.\n");
 
 /**
  * This file completely obliterates every course in Moodle.
