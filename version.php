993767f9906872407448c22c1519be1da0f58dbc
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-10-02 10:47:14 +0100

    Bump version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_kent';
-$plugin->version   = 2014100200;
+$plugin->version   = 2014100201;
 $plugin->requires  = 2014051200;
 $plugin->cron      = 0;
 
