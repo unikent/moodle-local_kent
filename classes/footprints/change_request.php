12ad6e2660cfad3153aef88126f5e37857cfa584
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-11-14 12:11:43 +0000

    Fix CR Ticket gen

--- classes/footprints/change_request.php
+++ classes/footprints/change_request.php
@@ -19,6 +19,7 @@ namespace local_kent\footprints;
 defined('MOODLE_INTERNAL') || die();
 
 require_once($CFG->dirroot . "/local/kent/lib/footprints/src/Ticket.php");
+require_once($CFG->dirroot . "/local/kent/lib/footprints/src/ChangeRequest.php");
 
 /**
  * Footprints ticket class.
