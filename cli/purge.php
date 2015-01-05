f3fb3afca3cfd1089e90d93f9b13dfc6032d2cd3
Skylar Kelty <S.Kelty@kent.ac.uk>
2015-01-05 12:50:55 +0000

    You sure?

--- cli/purge.php
+++ cli/purge.php
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+die("Ahem. You sure?");
+
 define('CLI_SCRIPT', true);
 
 require_once(dirname(__FILE__) . '/../../../config.php');
