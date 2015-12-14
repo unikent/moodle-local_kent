8baef5072ebcd396f52c4f4600b85680d996a3cd
Skylar Kelty <s.kelty@kent.ac.uk>
2015-12-14 16:34:22 +0000

    Cleanup

--- db/upgrade.php
+++ db/upgrade.php
@@ -874,5 +874,12 @@ function xmldb_local_kent_upgrade($oldversion) {
         upgrade_plugin_savepoint(true, 2015121400, 'local', 'kent');
     }
 
+    if ($oldversion < 2015121401) {
+        \local_kent\manager\abtest::finish_test('kent_theme_lightnav');
+
+        // Kent savepoint reached.
+        upgrade_plugin_savepoint(true, 2015121401, 'local', 'kent');
+    }
+
     return true;
 }
--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_kent';
-$plugin->version   = 2015121400;
+$plugin->version   = 2015121401;
 $plugin->requires  = 2015111600;
 
 $plugin->dependencies = array(
