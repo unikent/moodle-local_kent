f2726e7b7f9ddceb077388ecde40dd901337f713
Skylar Kelty <s.kelty@kent.ac.uk>
2016-02-29 16:47:11 +0000

    Fixes

--- setup.php
+++ setup.php
@@ -33,7 +33,7 @@ require_once(dirname(__FILE__) . "/../connect/classes/sds/sdsdb.php");
 $SDSDB = new \local_connect\sds\sdsdb();
 
 require_once(dirname(__FILE__) . "/../connect/classes/sits/sitsdb.php");
-$SITSDB = new \local_connect\sds\sitsdb();
+$SITSDB = new \local_connect\sits\sitsdb();
 
 if (!PHPUNIT_TEST || PHPUNIT_UTIL) {
     set_error_handler(function($errno, $errstr, $errfile, $errline, $errcontext) {
